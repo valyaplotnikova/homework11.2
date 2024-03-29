4fd1c4d20157e08a4b75480261a78e460a98f1f5
valya_plotnikova <valya_ezhova@mail.ru>
2024-03-29 10:50:41 +0300

    add new func and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_up(text):
+    '''Делает все буквы слов строки заглавными'''
     return text.upper()
+
+
+def get_first_lit_up(text):
+    '''Делает первые буквы слов строки заглавными'''
+    return text.title()
